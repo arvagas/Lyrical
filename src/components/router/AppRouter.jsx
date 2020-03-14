e0e1882e734bfff1300152616361c15024ec8fb7
Arvin Agas <arvagas@gmail.com>
2020-03-14 16:52:41 -0400

    tracking page views set up

--- src/components/router/AppRouter.jsx
+++ src/components/router/AppRouter.jsx
@@ -1,11 +1,26 @@
 // library imports
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Switch, Route, useLocation } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import ReactGA from 'react-ga'
 // component imports
 import Landing from './landing/Landing'
 
+// initialize google analytics
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID)
+const browserHistory = createBrowserHistory()
+browserHistory.listen((location, action) => {
+  ReactGA.pageview(location.pathname + location.search)
+})
 
 const AppRouter = () => {
+  const location = useLocation()
+
+  // will update ga every time a different page is navigated
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search)
+  }, [location])
+
   return (
     <Switch>
       <Route exact path='/' component={Landing} />
