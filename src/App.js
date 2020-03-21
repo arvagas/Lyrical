ba3b62ecb19d565a163810e33e25a1da5770395b
Arvin Agas <arvagas@gmail.com>
2020-03-20 23:42:23 -0400

    created simple footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react'
 // component imports
 import AppRouter from './components/router/AppRouter'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <AppRouter />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// library imports
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      Copyright 2020 Arvin Agas
+    </div>
+  )
+}
+
+export default Footer
